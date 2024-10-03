b449f6e764b89f5794c7cb7a4c215b2c1d9513fd
saikumar <saikumar10684@gmail.com>
2024-10-03 10:06:15 +0530

    dailytask-1 completed

---
+++ dailytask-1/index.js
@@ -0,0 +1,29 @@
+let num=1;
+let name="sai";
+let res=true;
+let a;
+let number=null;
+
+console.log(num,name,res,a,number);
+console.log(typeof num);
+console.log(typeof name);
+console.log(typeof res);
+console.log(typeof a);
+console.log(typeof number);
+
+let num1=20;
+let num2=10;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1%num2);
+console.log(num1/num2);
+
+console.log(num1==num2);
+console.log(num1===num2);
+console.log(num1!=num2);
+console.log(num1!==num2);
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(num1>=num2);
+console.log(num1<=num2);
